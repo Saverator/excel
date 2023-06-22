a444e3e196692ae2df7faf65eb83548457c5deda
Saverator <53902724+Saverator@users.noreply.github.com>
2023-06-22 22:04:36 +0300

    Resize (#4)
    * resize cols and rows
    
    * optimized resize cols and rows
    
    * add css method in dom.js
    
    * move resize code in table.resize.js
    
    * final edit

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,25 @@
-import {ExcelComponents} from '../../core/ExcelComponent';
-import {createTable} from './table.template';
+import {ExcelComponents} from '../../core/ExcelComponent'
+import {shouldResize} from './table.functions'
+import {tableResize} from './table.resize'
+import {createTable} from './table.template'
 
 export class Table extends ExcelComponents {
     static className = 'excel__table'
 
+    constructor($root) {
+        super($root, {
+            name: 'Table',
+            listeners: ['mousedown']
+        })
+    }
+
     toHTML() {
-        return createTable(100);
+        return createTable(20);
+    }
+
+    onMousedown(event) {
+        if (shouldResize(event)) {
+            tableResize(this.$root, event)
+        }
     }
 }
