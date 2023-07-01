28189c1f10d53e166cd9c2c9ae2297a7bb5d2115
Saverator <densan@bk.ru>
2023-07-01 19:13:49 +0300

    create class Store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import {Excel} from './components/excel/Excel'
 import './scss/index.scss'
-import {Header} from './components/header/Header';
-import {Toolbar} from './components/toolbar/Toolbar';
-import {Formula} from './components/formula/Formula';
-import {Table} from './components/table/Table';
+import {Header} from './components/header/Header'
+import {Toolbar} from './components/toolbar/Toolbar'
+import {Formula} from './components/formula/Formula'
+import {Table} from './components/table/Table'
+// import {createStore} from './core/createStore'
+import {rootReducer} from './redux/rootReducer'
+import {Store} from './core/Store'
+
+// const store = createStore(rootReducer)
+const store = new Store(rootReducer)
 
 const excel = new Excel('#app', {
-    components: [Header, Toolbar, Formula, Table]
+    components: [Header, Toolbar, Formula, Table],
+    store
 })
 
 excel.render()
